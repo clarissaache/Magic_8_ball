1724caf3f242469413cbe16999ba9d7f7064dc0c
EC2 Default User <ec2-user@ip-172-31-42-132.us-east-2.compute.internal>
2021-09-01 19:01:15 +0000

    Created scaffold and exposed it with API

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome! My name is Clarissa Ache.\n This is what I did for my first project in Data Ops class"}
+
+@app.get("/add/{name}")
+async def add(name: str):
+    """Enter a name and capitalize every letter"""
+    
+    name_c = name.capitalize()
+    return {"Your name is": name_c}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
