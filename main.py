ad72b950ea01de8ee9d1e33c8c2f39eb5af49ce2
Clarissa <clarissaache@gmail.com>
2021-09-09 20:28:54 +0000

    changed program to Noah's example

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    a = "Welcome! My name is Clarissa Ache.\n This is what I did for my first project in Data Ops class"
+    a = "Welcome! My name is Clarissa Ache.\n This is what I did for my first project in Data Engineering & Ops class"
     return {"message":a}
 
-@app.get("/program/mids")
-async def program(prog_name: str):
-    """Greet students"""
-        message = "Welcome MIDS Student!"
-    return {"message": message}
+@app.get("/add/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    """Add two numbers together"""
+
+    total = num1 + num2
+    return {"total": total}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
