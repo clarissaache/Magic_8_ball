579b8b9d5b79730d2ba3e2c94fe1afb20d02ccfa
Clarissa <clarissaache@gmail.com>
2021-09-09 20:16:04 +0000

    changed program name

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ app = FastAPI()
 async def root():
     return {"message": "Welcome! My name is Clarissa Ache.\n This is what I did for my first project in Data Ops class"}
 
-@app.get("/add/{name}")
-async def add(name: str):
+@app.get("/program/{prog_name}")
+async def program(prog_name: str):
     """Enter a name and capitalize first letter"""
-    
-    name_c = name.capitalize()
-    return {"Your name is": name_c}
+    if prog_name == 'mids'
+        message = "Welcome MIDS Student!"
+    else:
+        message = "Sorry! Your Master's program is not supported by this website"
+    return {"message"}
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
